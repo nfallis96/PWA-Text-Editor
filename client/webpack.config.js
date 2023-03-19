5fb57d1cebbd301f9786d51d19447ba98b755fca
nfallis96 <nfallis2019@fau.edu>
2023-03-19 17:54:19 -0400

    started the first js file and added plug ins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,9 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require("workbox-webpack-plugin");
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
@@ -18,8 +20,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+           new HtmlWebpackPlugin({
+            template: "./index.html",
+            title: "JATE",
+          }),
+          // css plugin extracts CSS
+          new MiniCssExtractPlugin(),
+          new InjectManifest({
+            swSrc: "./src-sw.js",
+            swDest: "src-sw.js",
+          }),
+          // build-up for manifest.json file.
+          new WebpackPwaManifest({
+            fingerprints: false,
+            inject: true,
+            name: "Just Another Text Editor",
+            short_name: "JATE",
+            description: "Just another Text Editor",
+            background_color: "#225ca3",
+            theme_color: "#225ca3",
+            start_url: "./",
+            publicPath: "./",
+            icons: [
+              {
+                src: path.resolve("src/images/logo.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons"),
+              },
+            ],
+          }),
+        ],
 
     module: {
       rules: [
